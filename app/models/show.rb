7e08f55b47138601bcb6134076c09158486aa701
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 02:12:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,16 +1,2 @@
 class Show < ActiveRecord::Base 
-  
-  def highest_rating 
-    Show.maximum(:rating)
-  end 
-  
-  def most_popular_show 
-  end 
-  
-  def lowest_rating
-  end 
-  
-  def least_popular_show
-  end 
-  
 end 
